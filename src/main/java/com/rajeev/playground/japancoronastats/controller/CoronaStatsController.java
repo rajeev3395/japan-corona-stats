601e2addc75fe7d3d6b1abcdedc480e7b0d540c1
Rajeev Verma <rverma@yahoo-corp.jp>
2020-04-04 17:56:11 +0900

    Add corona stats controller

--- src/main/java/com/rajeev/playground/japancoronastats/controller/CoronaStatsController.java
+++ src/main/java/com/rajeev/playground/japancoronastats/controller/CoronaStatsController.java
@@ -1,9 +1,23 @@
 package com.rajeev.playground.japancoronastats.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.rajeev.playground.japancoronastats.contants.ApplicationConstants.CORONA_STATS;
+
+@Slf4j
+@RequiredArgsConstructor
 @RestController
-public class CoronaSatatsController {
+public class CoronaStatsController {
 
-
+    @GetMapping(value = CORONA_STATS, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getCoronaStatistics(@PathVariable(value = "prefecture") String prefectureName) {
+        log.debug("Received the stats request for prefecture : {}", prefectureName);
+        return ResponseEntity.ok().build();
+    }
 }
