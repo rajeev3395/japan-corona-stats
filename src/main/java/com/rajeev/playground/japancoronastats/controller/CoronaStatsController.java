1550ea291a32adca15947c2fe2227dd5339d466f
Rajeev Verma <rverma@yahoo-corp.jp>
2020-04-04 15:40:36 +0900

    Add lombok dependency

---
+++ src/main/java/com/rajeev/playground/japancoronastats/controller/CoronaStatsController.java
@@ -0,0 +1,9 @@
+package com.rajeev.playground.japancoronastats.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoronaSatatsController {
+
+
+}
