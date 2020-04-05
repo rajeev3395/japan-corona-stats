5e0ab59c50c3ba7eadadbfbbebead6c17544a664
Rajeev Verma <rverma@yahoo-corp.jp>
2020-04-05 14:52:01 +0900

    Add default constructor

--- src/main/java/com/rajeev/playground/japancoronastats/dao/CoronaDetailsEntity.java
+++ src/main/java/com/rajeev/playground/japancoronastats/dao/CoronaDetailsEntity.java
@@ -5,23 +5,27 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "corona_details")
 public class CoronaDetailsEntity {
     @Id
     @Column(name = "prefecture_name")
-    private final String prefectureName;
-    private final Long tested;
-    private final Long confirmed;
-    private final Long recovered;
-    private final Long active;
-    private final Long critical;
-    private final Long deaths;
+    private String prefectureName;
+    private Long tested;
+    private Long confirmed;
+    private Long recovered;
+    private Long active;
+    private Long critical;
+    private Long deaths;
     @Column(name = "created_at")
-    private final Timestamp createdAt;
+    private Timestamp createdAt;
     @Column(name = "updated_at")
-    private final Timestamp updatedAt;
+    private Timestamp updatedAt;
 }
